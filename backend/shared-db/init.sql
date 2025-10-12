254e7b6dc19d095d9d870fbad468f16d25db0331
GC; Pins <spot.meow.meow@gmail.com>
2025-10-12 18:43:06 -0400

    fixed DB schema

--- backend/shared-db/init.sql
+++ backend/shared-db/init.sql
@@ -10,12 +10,3 @@ CREATE TABLE Event (
     event_location TEXT NOT NULL,
     event_capacity INTEGER --ticket capacity
 );
-
-DROP TABLE IF EXISTS Ticket;
-CREATE TABLE Ticket (
-    ticket_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    event_id INTEGER NOT NULL,
-    ticket_price REAL,
-    ticket_type TEXT,
-    FOREIGN KEY (event_id) REFERENCES Event(event_id)
-);
